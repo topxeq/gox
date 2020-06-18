b20db56dfacddabaf7812edd61d470c3cb5f7a6c
unknown <topxeq@163.com>
2020-06-18 14:09:18 +0800

    2020061801

---
+++ gui_giu_nongiugui.go
@@ -0,0 +1,21 @@
+// +build !giugui
+
+package main
+
+// GUI related start
+
+// GUI related end
+
+func InitGiu() {
+}
+
+// full version related start
+
+func loadFont() {
+}
+
+// full version related end
+
+func InitGiuExports() {
+
+}
