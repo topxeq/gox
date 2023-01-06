fda984410c19b88993033ae18234545fbb3c4387
unknown <topxeq@163.com>
2023-01-06 16:56:10 +0800

    2023010601

--- webview2_windows.go
+++ webview2_windows.go
@@ -13,10 +13,11 @@ func newWebView2(optsA ...string) interface{} {
 	widthT := uint(tk.ToInt(tk.GetSwitch(optsA, "-width=", "800"), 800))
 	heightT := uint(tk.ToInt(tk.GetSwitch(optsA, "-height=", "600"), 600))
 	iconT := uint(tk.ToInt(tk.GetSwitch(optsA, "-icon=", "2"), 2))
-	centerT := !tk.IfSwitchExistsWhole(optsA, "-centerFalse")
+	centerT := tk.IfSwitchExistsWhole(optsA, "-center")
+	debugT := tk.IfSwitchExistsWhole(optsA, "-debug")
 
 	w := webview2.NewWithOptions(webview2.WebViewOptions{
-		Debug:     true,
+		Debug:     debugT,
 		AutoFocus: true,
 		WindowOptions: webview2.WindowOptions{
 			Title:  titleT,
